cbae21180a1251ae94288109c7ae8743f48deb90
Mike Ross <m.ross@me.com>
2015-11-03 15:27:19 -0500

    Added startup commands for web app

--- mongodb-custom/recipes/default.rb
+++ mongodb-custom/recipes/default.rb
@@ -7,3 +7,11 @@ end
 execute 'create_indexes' do
   command 'mongo /vagrant/scripts/createMongoIndexes.js'
 end
+
+execute 'install_packages' do
+  command 'cd /vagrant/mongo-express && npm install'
+end
+
+execute 'run_app' do
+  command 'cd /vagrant/mongo-express && nohup npm start > mongoApp.log &'
+end
