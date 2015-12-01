0f1aa4bcb19c4fa6dd51f0244a8409ecbb71db59
Mike Ross <m.ross@me.com>
2015-12-01 10:17:39 -0500

    Refactored where clause, added facet filtering

--- couchbase-express/routes/posts.js
+++ couchbase-express/routes/posts.js
@@ -7,6 +7,14 @@ var bucket = cluster.openBucket('cvm');
 // Setup Query
 var N1qlQuery = couchbase.N1qlQuery;
 
+function whereClause (tagParam) {
+	var whereClause = tagParam.split(',').map(function (tag, k) {
+			return (k > 0) ? ' AND "' + tag + '" IN cvm.categories' : ' WHERE "' + tag + '" IN cvm.categories';
+		});
+
+	return (whereClause + '').replace(/,/g, '');
+}
+
 
 /*
  * List posts
@@ -14,21 +22,17 @@ var N1qlQuery = couchbase.N1qlQuery;
  */
 router.get('/', function (req, res) {
 	var options = {
-		criteria: {},
 		perPage: parseInt(req.query.size) || 10,
 		page: parseInt(req.query.page) || 1
 	};
 
-	var str = 'SELECT title, author, date, categories, description FROM `cvm` ';
+	var str = 'SELECT DISTINCT title, author, date, categories, description FROM `cvm` ';
+
 	if (req.query.tag) {
-		var whereClause = req.query.tag.split(',').map(function (tag, k) {
-			return (k > 0) ? ' AND "' + tag + '" IN categories' : ' WHERE "' + tag + '" IN categories';
-		});
-		whereClause = (whereClause + '').replace(/,/g, '');
-		str += whereClause;
+		str += whereClause(req.query.tag);
 	}
 
-	var pagination = (' LIMIT ' + options.perPage + ' OFFSET ' + (options.page * options.perPage - options.perPage));
+	var pagination = (' GROUP BY title ORDER BY date DESC LIMIT ' + options.perPage + ' OFFSET ' + (options.page * options.perPage - options.perPage));
 	str += pagination;
 
 	console.log('Query: ' + str);
@@ -50,21 +54,18 @@ router.get('/', function (req, res) {
  */
 router.get('/facets', function (req, res) {
 	var options = {
-		criteria: {},
 		perPage: parseInt(req.query.size) || 10
 	};
 
-	var str = 'SELECT _id, COUNT(*) as count FROM `cvm` UNNEST categories as _id GROUP BY _id ORDER BY count DESC';
-	// if (req.query.tag) {
-	// 	var whereClause = req.query.tag.split(',').map(function (tag, k) {
-	// 		return (k > 0) ? ' AND "' + tag + '" IN categories' : ' WHERE "' + tag + '" = tag';
-	// 	});
-	// 	whereClause = (whereClause + '').replace(/,/g, '');
-	// 	str += whereClause;
-	// }
+	var str = 'SELECT _id, COUNT(*) as count FROM `cvm` UNNEST categories as _id ';
 
+	if (req.query.tag) {
+		str += whereClause(req.query.tag);
+	}
+
+	var aggregation = ' GROUP BY _id ORDER BY count DESC ';
 	var pagination = ' LIMIT ' + options.perPage;
-	str += pagination;
+	str += aggregation + pagination;
 
 	console.log('Query: ' + str);
 	var query = N1qlQuery.fromString(str);
